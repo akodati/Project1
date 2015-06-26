749a0828589f91ad5898e68e035f8081fcbbca79
Ashwini Kodati <Ashu@ashwinis-mbp.attlocal.net>
2015-06-25 21:36:11 -0500

    Adding new controller

--- src/main/java/com/codetutr/springconfig/WebConfig.java
+++ src/main/java/com/codetutr/springconfig/WebConfig.java
@@ -1,9 +1,11 @@
 package com.codetutr.springconfig;
- 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
  
 @Configuration
@@ -11,6 +13,16 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages="com.codetutr.controller")
 public class WebConfig {
  
+	/**
+	 * Since we don't have any controller logic, simpler to just
+	 * define controller for page using View Controller. Note:
+	 * had to extend WebMvcConfigurerAdapter to get this functionality
+	 * @param registry
+	 */
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+	
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = 
